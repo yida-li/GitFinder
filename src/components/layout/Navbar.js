5db09661866587e9bebb83a6104dc69211d6e973
Gabriel <Gabrielyidali@gmail.com>
2020-09-23 15:03:57 -0400

    finished chapter 15

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: " fab fa-github",
-  };
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <div>
-        <nav className='navbar bg-primary'>
-          <h1>
-            <i className={this.props.icon} />
-            {this.props.title}
-          </h1>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <div>
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={icon} />
+          {title}
+        </h1>
+      </nav>
+    </div>
+  );
+};
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: " fab fa-github",
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
