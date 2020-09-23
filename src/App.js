708312ae062058815b12d5bb72f3fd92e59841d2
Gabriel <Gabrielyidali@gmail.com>
2020-09-23 10:22:02 -0400

    finished chapter 10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello Yida</h1>
+        <Navbar />
       </div>
     );
   }
