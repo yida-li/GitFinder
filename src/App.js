5ae521e4bfed0e0b446f1dd110b256a39530a5ec
Gabriel <Gabrielyidali@gmail.com>
2020-10-01 13:10:50 -0400

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
+import './App.css';
+import Navbar from './components/layout/Navbar';
 
-import Users from "./components/users/Users";
-import axios from "axios";
+import Users from './components/users/Users';
+import axios from 'axios';
+
 class App extends Component {
   state = { users: [], loading: false };
 
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
-    console.log(res.data);
   }
+
   render() {
     return (
       <div className='App'>
