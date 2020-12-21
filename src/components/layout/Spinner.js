87b243740087b683a2f58134d97489867f192fa2
yida-li <gabrielyidali@gmail.com>
2020-12-21 17:44:55 -0500

    new start

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,11 +4,12 @@ const Spinner = () => {
   return (
     <Fragment>
       <img
-        src={spinner}
-        alt='System Processing...'
+        src={spinner} 
+        alt='System Processing...'   // displaying a gif when an action is requested which is very useful here
         style={{ width: "200px", margin: "auto", display: "block" }}
       />
     </Fragment>
   );
 };
+
 export default Spinner;
